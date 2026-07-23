491919e863888deee644fdbc57aba4b18e792c6d
mikecorey <mike.corey@gmail.com>
2026-07-23 18:59:23 +0000

    updates address book

--- py101_ex/address_book.py
+++ py101_ex/address_book.py
@@ -16,6 +16,41 @@ matt_contact = {
 }
 contacts.append(matt_contact)
 
+alice_contact = {
+    "name": "Alice",
+    "phone": "555-123-4567",
+    "email": "alice@example.com"
+}
+contacts.append(alice_contact)
+
+bob_contact = {
+    "name": "Bob",
+    "phone": "555-987-6543",
+    "email": "bob@example.com"
+}
+contacts.append(bob_contact)
+
+carol_contact = {
+    "name": "Carol",
+    "phone": "555-555-1234",
+    "email": "carol@example.com"
+}
+contacts.append(carol_contact)
+
+doug_contact = {
+    "name": "Doug",
+    "phone": "555-555-5678",
+    "email": "doug@example.com"
+}
+contacts.append(doug_contact)
+
+eve_contact = {
+    "name": "Eve",
+    "phone": "555-555-8765",
+    "email": "eve@example.com"
+}
+contacts.append(eve_contact)
+
 # Add a bad contact
 bad_contact = {
     "name": "Bad Contact",
@@ -42,3 +77,11 @@ contacts.remove(bad_contact)
 for contact in contacts:
     print(f"Name: {contact['name']}, Phone: {contact['phone']}, Email: {contact['email']}") 
 
+mike_contact['is_favorite'] = True
+bob_contact['is_favorite'] = True
+eve_contact['is_favorite'] = True
+matt_contact['is_favorite'] = True
+
+for f in contacts:
+    if 'is_favorite' in f and f['is_favorite']:
+        print(f"{f['name']} is a favorite contact.")
